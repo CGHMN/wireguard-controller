e79c43a476af81659fd82903be8b2df1157bd74a
jonas.luehrig <github@diskcat.com>
2025-09-28 23:58:49 +0200

    Added PersistentKeepalive to peer config generation

--- app/Connectors/WireguardConnector.php
+++ app/Connectors/WireguardConnector.php
@@ -57,6 +57,44 @@ class WireguardConnector
 	}
 
 	/**
+	 * Creates a configuration file for one of the server peers
+	 * @param \App\Models\Server $server Instance of Server model to generate peer config with
+	 * @param string $peer_id Database ID of Peer to generate peer config for
+	 * @return string Wireguard configuration as multi-line string
+	 */
+	public static function generate_peer_config(Server $server, string $peer_id): string
+	{
+		$peer = Peer::where('server_id', $server->id)->findOrFail($peer_id);
+		$server_tunnel_network = IpAddressHelper::network_address_from_cidr(
+			$server->tunnel_ip
+		);
+
+		$allowed_ips = [
+			$server_tunnel_network,
+			$server->routed_subnet
+		];
+
+		foreach ($server->allowed_ips as $ip) {
+			$allowed_ips[] = $ip->cidr;
+		}
+
+		$allowed_ips_str = join(', ', $allowed_ips);
+
+		return <<<PEER_CONFIG
+		[Interface]
+		Address = {$peer->tunnel_ip}
+		PrivateKey = <insert-private-key-here>
+
+		[Peer]
+		PublicKey = {$server->public_key}
+		AllowedIPs = {$allowed_ips_str}
+		Endpoint = {$server->endpoint}:{$server->listen_port}
+		PersistentKeepalive = 15
+		\n
+		PEER_CONFIG;
+	}
+
+	/**
 	 * Creates a Wireguard interface and assigns IP addresses and routes
 	 * 
 	 * @param \App\Models\Server $server Server instance to bring online
@@ -93,42 +131,6 @@ class WireguardConnector
 		Process::input(self::generate_server_config($server))
 			->run("sudo /usr/bin/wg setconf '{$server->interface_name}' /dev/stdin")
 			->throw();
-	}
-
-	/**
-	 * Creates a configuration file for one of the server peers
-	 * @param \App\Models\Server $server Instance of Server model to generate peer config with
-	 * @param string $peer_id Database ID of Peer to generate peer config for
-	 * @return string Wireguard configuration as multi-line string
-	 */
-	public static function generate_peer_config(Server $server, string $peer_id): string {
-		$peer = Peer::where('server_id', $server->id)->findOrFail($peer_id);
-		$server_tunnel_network = IpAddressHelper::network_address_from_cidr(
-			$server->tunnel_ip
-		);
-
-		$allowed_ips = [
-			$server_tunnel_network,
-			$server->routed_subnet
-		];
-
-		foreach($server->allowed_ips as $ip) {
-			$allowed_ips[] = $ip->cidr;
-		}
-
-		$allowed_ips_str = join(', ', $allowed_ips);
-
-		return <<<PEER_CONFIG
-		[Interface]
-		Address = {$peer->tunnel_ip}
-		PrivateKey = <insert-private-key-here>
-
-		[Peer]
-		PublicKey = {$server->public_key}
-		AllowedIPs = {$allowed_ips_str}
-		Endpoint = {$server->endpoint}:{$server->listen_port}
-		\n
-		PEER_CONFIG;
 	}
 
 	/**
