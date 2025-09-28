7185b33fcd6457402705c4d6baae62a55d62084c
jonas.luehrig <github@diskcat.com>
2025-09-28 23:46:22 +0200

    Added more sensible example routed subnet entries for seeded peers

--- database/seeders/PeerAllowedIpSeeder.php
+++ database/seeders/PeerAllowedIpSeeder.php
@@ -14,13 +14,27 @@ class PeerAllowedIpSeeder extends Seeder
      */
     public function run(): void
     {
-		$peer = Peer::firstOrFail();
+		$peer = Peer::findOrFail(1);
 
-		foreach(['192.168.0.0/24', '100.99.98.0/24'] as $cidr) {
+		foreach(['192.168.0.0/24', '192.168.1.0/24'] as $cidr) {
 			PeerAllowedIp::create([
 				'cidr' => $cidr,
 				'peer_id' => $peer->id
 			]);
 		}
+
+		$peer = Peer::findOrFail(2);
+
+		PeerAllowedIp::create([
+			'cidr' => '192.168.2.0/24',
+			'peer_id' => $peer->id
+		]);
+
+		$peer = Peer::findOrFail(3);
+
+		PeerAllowedIp::create([
+			'cidr' => '192.168.3.0/24',
+			'peer_id' => $peer->id
+		]);
     }
 }
