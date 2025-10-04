c253f746f084ec601865c7107760364cced82b3f
jonasluehrig <github@diskcat.com>
2025-10-04 04:43:40 +0000

    Add migrations to make delete actions remove child entries as well

---
+++ database/migrations/2025_10_04_035633_alter_peers_table_cascade_on_delete.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('peers', function (Blueprint $table) {
+			// $table->dropForeign('user_id');
+			// $table->dropForeign('server_id');
+
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->nullable()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+
+			$table->foreign('server_id')
+				->references('id')
+				->on('servers')
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
